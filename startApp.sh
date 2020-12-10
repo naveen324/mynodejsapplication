19e85d85d0fe10ece50785330adfa643ececde13
naveen324 <67173870+naveen324@users.noreply.github.com>
2020-12-09 23:08:32 -0600

    Update startApp.sh

--- startApp.sh
+++ startApp.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export NODE_ENV=production
-export DB_PRD_HOST=postgres.czoymgtrv9vo.us-east-2.rds.amazonaws.com
+export DB_PRD_HOST=
 export DB_PRD_USER=postgres
-export DB_PRD_PASS=sairam324
+export DB_PRD_PASS=
 export NODE_HOST=localhost
 export NODE_PORT=8080
 node /myapp/index.js&
